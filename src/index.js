371b25d066b827275ea5205345e17a27ad109f65
Noah Mustoe <62711423+cobradev4@users.noreply.github.com>
2023-01-28 15:06:55 -0600

    setup search page

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import Search from './pages/Search/Search';
+
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App/>
+    element: <Search/>
   }
 ])
 
@@ -20,7 +21,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
