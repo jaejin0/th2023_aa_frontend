2df79f18ec71ae3788f08006a211716cfa54a6f7
Noah Mustoe <62711423+cobradev4@users.noreply.github.com>
2023-01-28 21:01:08 -0600

    search page layout (minus responsiveness)

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,34 +1,59 @@
 import React, {useState} from 'react';
-import DatePicker from 'react-datepicker';
+import dayjs from 'dayjs';
+import { TextField } from '@mui/material';
+import Button from '@mui/material/Button';
+
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+
+import { ReactComponent as Plane } from '../../img/plane.svg';
+import { ReactComponent as Cloud1 } from '../../img/cloud1.svg';
+import { ReactComponent as Cloud2 } from '../../img/cloud2.svg';
+import { ReactComponent as Cloud3 } from '../../img/cloud3.svg';
+import { ReactComponent as Cloud4 } from '../../img/cloud4.svg';
 
 import './styles.scss';
-import 'react-datepicker/dist/react-datepicker.css';
 
 const Search = () => {
     const [flightNumber, setFlightNumber] = useState();
-    const [date, setDate] = useState(new Date());
+    const [date, setDate] = useState(dayjs('2023-01-28T21:11:54'));
 
     return (
-        <div className='search-page'>
-            <h1>Airport Planner</h1>
-            <div className='search-box'>
-                <h3>Enter your flight information to get started</h3>
-                <div className='flight-info'>
+        <LocalizationProvider dateAdapter={AdapterMoment}>
+            <div className='search-page'>
+                <h1>FLIGHT CATCHER</h1>
+
+                <div className='info-container'>
                     <div className='flight-number'>
-                        <input
-                            onChange={(e) => {setFlightNumber(e.target.value)}}
-                            placeholder='Flight Number'
+                        <p>Flight Number</p>
+                        <TextField 
+                            fullWidth
+                            color='primary'
+                            onChange={(e) => setFlightNumber(e.target.value)}
                         />
                     </div>
                     <div className='date'>
-                        <DatePicker
-                            selected={date} 
+                        <p>Date</p>
+                        <DesktopDatePicker
+                            fullWidth
+                            value={date}
                             onChange={(date) => setDate(date)}
+                            renderInput={(params) => <TextField {...params} fullWidth/>}
                         />
                     </div>
+                    <div className='submit'>
+                        <Button variant='contained'>Get Info</Button>
+                    </div>
                 </div>
+
+                <Plane className='plane'/>
+                <Cloud1 className='cloud1'/>
+                <Cloud2 className='cloud2'/>
+                <Cloud3 className='cloud3'/>
+                <Cloud4 className='cloud4'/>
             </div>
-        </div>
+        </LocalizationProvider>
     );
 };
 
