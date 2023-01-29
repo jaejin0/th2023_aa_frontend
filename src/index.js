4a6a74f07fe38fb7741bd489929d34fde350afe7
Noah Mustoe <62711423+cobradev4@users.noreply.github.com>
2023-01-29 01:29:26 -0600

    Add animations and flight details

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 import Search from './pages/Search/Search';
+import FlightDetails from './pages/FlightDetails/FlightDetails';
 
 import './index.scss';
+import 'react-toastify/dist/ReactToastify.css';
+
 import reportWebVitals from './reportWebVitals';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Search/>
+  },
+  {
+    path: '/catch/:flightNum',
+    element: <FlightDetails/>
   }
 ])
 
@@ -18,6 +26,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router}/>
+    <ToastContainer/>
   </React.StrictMode>
 );
 
--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,4 +1,6 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import dayjs from 'dayjs';
 import { TextField } from '@mui/material';
 import Button from '@mui/material/Button';
@@ -16,12 +18,35 @@ import { ReactComponent as Cloud4 } from '../../img/cloud4.svg';
 import './styles.scss';
 
 const Search = () => {
-    const [flightNumber, setFlightNumber] = useState();
+    const [flightNumber, setFlightNumber] = useState(null);
     const [date, setDate] = useState(dayjs('2023-01-28T21:11:54'));
+    const [submit, setSubmit] = useState(false);
+    const [loaded, setLoaded] = useState(false);
+    const navigate = useNavigate();
+
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+
+    const onSubmit = async () => {
+        if (flightNumber != null && flightNumber != '') {
+            setSubmit(true);
+            await delay(1500); // wait for animation to complete then redirect
+            navigate(`/catch/${flightNumber}?date=${date.unix()}`);
+        } else
+            toast.error('Please enter a flight number!');
+    };
+
+    const delayLoad = async () => {
+        await delay(750);
+        setLoaded(true);
+    }
+
+    useEffect(() => {
+        delayLoad();
+    }, []);
 
     return (
         <LocalizationProvider dateAdapter={AdapterMoment}>
-            <div className='search-page'>
+            <div className={!loaded || submit ? 'search-page hide' : 'search-page'}>
                 <h1>FLIGHT CATCHER</h1>
 
                 <div className='info-container'>
@@ -43,15 +68,15 @@ const Search = () => {
                         />
                     </div>
                     <div className='submit'>
-                        <Button variant='contained'>Get Info</Button>
+                        <Button onClick={onSubmit} variant='contained'>Get Info</Button>
                     </div>
                 </div>
 
-                <Plane className='plane'/>
-                <Cloud1 className='cloud1'/>
-                <Cloud2 className='cloud2'/>
-                <Cloud3 className='cloud3'/>
-                <Cloud4 className='cloud4'/>
+                <Plane className={!loaded || submit ? 'plane out' : 'plane in'}/>
+                <Cloud1 className={!loaded || submit ? 'cloud1 out' : 'cloud1 in'}/>
+                <Cloud2 className={!loaded || submit ? 'cloud2 out' : 'cloud2 in'}/>
+                <Cloud3 className={!loaded || submit ? 'cloud3 out' : 'cloud3 in'}/>
+                <Cloud4 className={!loaded || submit ? 'cloud4 out' : 'cloud4 in'}/>
             </div>
         </LocalizationProvider>
     );
